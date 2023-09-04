42f389d3a700e2413a78806d87b74fba679c737b
Edwin <edwin.correo.g@gmail.com>
2023-09-04 17:53:23 -0400

    vid 142 ide general de reducer

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -13,9 +13,10 @@ import './index.css'
 // import { MemorizeHook } from './06-memos/MemorizeHook'
 // import { CallBackHook } from './06-memos/CallBackHook'
 import { Padre } from './07-tarea-memo/Padre'
+import  './08-useReducer/intro-reducer'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <Padre />
-  // </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//     <Padre />
+//   // </React.StrictMode>,
+// )
