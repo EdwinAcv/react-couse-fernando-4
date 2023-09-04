2261c34595496e9c8de68a40cc0179597d773158
Edwin <edwin.correo.g@gmail.com>
2023-09-04 11:21:50 -0400

    uso de useRef

--- hook-app/src/04-useRef/FokusScree.jsx
+++ hook-app/src/04-useRef/FokusScree.jsx
@@ -1,16 +1,28 @@
+import { useRef } from "react"
 
 
 export const FokusScree = () => {
+
+    const onClick = () => {
+        inputRef.current.select();
+    }
+
+    const inputRef = useRef()
   return (
     <>
         <h1>Focus Screen</h1>
         <hr/>
 
         <input
+            ref={ inputRef }
             type="text"
             placeholder="Ingrese su nombre"
             className="form-control"
         />
+
+        <button onClick={ onClick } className="btn btn-primary mt-2">
+            Focus
+        </button>
     </>
   )
 }
