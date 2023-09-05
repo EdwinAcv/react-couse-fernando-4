ed794a21ff9662446eb249d74d2c482516d302f5
Edwin <edwin.correo.g@gmail.com>
2023-09-05 09:57:50 -0400

    ved 144 TodoList

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -12,11 +12,12 @@ import './index.css'
 // import { Memorize } from './06-memos/Memorize'
 // import { MemorizeHook } from './06-memos/MemorizeHook'
 // import { CallBackHook } from './06-memos/CallBackHook'
-import { Padre } from './07-tarea-memo/Padre'
-import  './08-useReducer/intro-reducer'
+// import { Padre } from './07-tarea-memo/Padre'
+// import  './08-useReducer/intro-reducer'
+import { TodoApp } from './08-useReducer/TodoApp'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   // <React.StrictMode>
-//     <Padre />
-//   // </React.StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <TodoApp />
+  // </React.StrictMode>,
+)
