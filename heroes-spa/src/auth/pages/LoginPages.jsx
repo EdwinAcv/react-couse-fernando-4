916dcdd74d472ae2796c037a9ac81ddd93d58721
Edwin <edwin.correo.g@gmail.com>
2023-09-07 09:49:35 -0400

    Agregando la primera ruta a heroesApp

---
+++ heroes-spa/src/auth/pages/LoginPages.jsx
@@ -0,0 +1,9 @@
+
+
+export const LoginPages = () => {
+  return (
+    <div>
+      LoginPage
+    </div>
+  )
+}
---
+++ heroes-spa/src/router/AppRouter.jsx
@@ -0,0 +1,18 @@
+import { Navigate, Route,Routes } from 'react-router-dom'
+import { MarvelPage } from '../heroes/pages/MarvelPage'
+import { LoginPages } from '../auth/pages/LoginPages'
+import { DcPage } from '../heroes/pages/DcPage'
+
+export const AppRouter = () => {
+  return (
+    <>
+        <Routes>
+            <Route path='marvel' element={ <MarvelPage/> } />
+            <Route path='dc' element={ <DcPage/> } />
+
+            <Route path='login' element={ <LoginPages/> } />
+            <Route path='/' element={ <Navigate to="/marvel" /> } />
+        </Routes>
+    </>
+  )
+}
