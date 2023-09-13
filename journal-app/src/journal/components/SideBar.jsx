c1cdfd3dddca390c9858c0805800c9d33c4a63b3
Edwin <edwin.correo.g@gmail.com>
2023-09-13 11:31:33 -0400

    vid 249 Creando sidebar

---
+++ journal-app/src/journal/components/SideBar.jsx
@@ -0,0 +1,48 @@
+import { TurnedInNot } from "@mui/icons-material"
+import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+
+
+export const SideBar = ( { drawerWidth = 240 } ) => {
+  return (
+    <Box
+        component='nav'
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+        <Drawer
+            variant='permanent'
+            open
+            sx={{
+                display: { xs: 'block' },
+                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+            }}
+        >
+            <Toolbar>
+                <Typography variant="h6" noWrap component='div'>
+                    Edwin Acevedo
+                </Typography>
+            </Toolbar>
+            <Divider />
+
+            <List>
+                {
+                    ['Enero','Febrero','Marzo','Abril'].map( text => (
+                        <ListItem key={ text } disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <TurnedInNot />
+                                </ListItemIcon>
+                                <Grid>
+                                    <ListItemText primary={ text }/>
+                                    <ListItemText secondary={ 'Esto es un texto creado con el objetio de que sea una prueba' }/>
+                                </Grid>
+                            </ListItemButton>
+                            
+
+                        </ListItem>
+                    ) )
+                }
+            </List>
+        </Drawer>
+    </Box>
+  )
+}
--- journal-app/src/journal/layout/JournalLayout.jsx
+++ journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material"
-import { NavBar } from "../components/NavBar";
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 240;
 
@@ -11,12 +11,12 @@ export const JournalLayout = ( { children } ) => {
         <NavBar drawerWidth= { drawerWidth }/>
 
         {/* Sidebar */}
-
+        <SideBar drawerWidth= { drawerWidth }/>
         <Box
             component='main'
             sx={ { flexGrow: 1, p: 3 } }
         >
-            {/* Toolbar */}
+            <Toolbar />
 
             { children }
         </Box>
