d187ae09fdad5ec45f64a77f4bc5e05ef6e7c14f
Edwin <edwin.correo.g@gmail.com>
2023-09-13 10:54:31 -0400

    vid 248 creacion de NavBar

--- journal-app/src/journal/layout/JournalLayout.jsx
+++ journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,10 +1,14 @@
 import { Box } from "@mui/material"
+import { NavBar } from "../components/NavBar";
+
+const drawerWidth = 240;
 
 
 export const JournalLayout = ( { children } ) => {
   return (
     <Box sx={ { display: 'flex' } }>
         {/* Navvbar */}
+        <NavBar drawerWidth= { drawerWidth }/>
 
         {/* Sidebar */}
 
