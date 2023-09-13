8b5e7d9673bf085ac35fc6d017db62b39edf79dc
Edwin <edwin.correo.g@gmail.com>
2023-09-13 10:09:45 -0400

    journalLayout y journalPage

--- journal-app/src/journal/pages/JournalPage.jsx
+++ journal-app/src/journal/pages/JournalPage.jsx
@@ -1,9 +1,12 @@
 import { Typography } from "@mui/material"
-
+import { JournalLayout } from "../layout/JournalLayout"
 
 
 export const JournalPage = () => {
   return (
-    <Typography variant="h1"> JournalPage </Typography>
+    <JournalLayout>
+      <Typography > In anim consequat ullamco eu adipisicing reprehenderit occaecat. Sint dolor laborum aliquip in labore eu qui ex. Pariatur veniam sit sunt voluptate nisi tempor laboris voluptate eiusmod et. Consectetur culpa cupidatat qui velit ullamco quis dolore pariatur pariatur in non. Non Lorem nulla amet ex aliqua sunt. Elit et in dolore eu reprehenderit. Enim adipisicing reprehenderit ullamco elit. Deserunt enim et aliquip consequat cupidatat ipsum velit aliqua reprehenderit magna eu. Minim mollit cillum irure aute nostrud sint in excepteur aliqua ut adipisicing in id veniam. 
+        Consectetur enim ex anim magna in irure minim in reprehenderit. Est ea Lorem voluptate amet aute ea laborum consequat magna dolore incididunt aute. Cupidatat veniam minim irure consequat minim. Velit mollit id minim duis ex id exercitation amet. Non ad deserunt dolore enim qui. Et nulla cupidatat duis sunt esse officia. Deserunt aliqua exercitation exercitation et dolor fugiat. </Typography>
+    </JournalLayout>
   )
 }
