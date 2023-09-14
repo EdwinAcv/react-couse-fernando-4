e090a3ae5c554998293ddd3734af389733b0de71
Edwin <edwin.correo.g@gmail.com>
2023-09-14 15:36:23 -0400

    fin seccion 18 rtk query desde vid 268 haasta 270

--- toolkit-redux/src/store/apis/todosApi.js
+++ toolkit-redux/src/store/apis/todosApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
  
-export const todosApi = () => createApi({
+export const todosApi = createApi({
     reducerPath: 'todos',
 
     baseQuery: fetchBaseQuery({
@@ -10,11 +10,14 @@ export const todosApi = () => createApi({
     endpoints: ( builder ) => ({
         getTodos: builder.query({
             query: () => '/todos'
+        }),
+        getTodo: builder.query({
+            query: (todoId) => `/todos/${ todoId }`
         })
     })
 })
 
 
-export const { useGetTodosQuery } = todosApi;
+export const { useGetTodosQuery, useGetTodoQuery} = todosApi;
 
 
