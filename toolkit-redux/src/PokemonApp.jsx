434f40ec4367f5562ca6c0b7f68f7116343ed0e9
Edwin <edwin.correo.g@gmail.com>
2023-09-14 13:35:43 -0400

    mostrar pokemosPaginadamente

--- toolkit-redux/src/PokemonApp.jsx
+++ toolkit-redux/src/PokemonApp.jsx
@@ -1,27 +1,40 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux"
-import { getPokemons } from "./store/slices/pokemon";
+import { useDispatch, useSelector } from "react-redux"
+import { getPokemons, startLoadingPokemons } from "./store/slices/pokemon";
 
 
 export const PokemonApp = () => {
 
+  
   const dispatch = useDispatch();
+  const { pokemons, page ,isLoading } = useSelector( state => state.pokemons );
 
   useEffect(() => {
     dispatch( getPokemons() );
   }, [])
-  
 
   return (
     <>
         <h1>PokemonApp</h1>
         <hr />
 
+        <span> Loading: { isLoading ? 'True': 'False' } </span>
+
         <ul>
-            <li>Hola</li>
-            <li>Hola</li>
-            <li>Hola</li>
+          {
+            pokemons.map( pokemon => (
+              <li key={ pokemon.name }> { pokemon.name } </li>
+
+            ) )
+          }
         </ul>
+
+        <button
+          disabled = { isLoading  }
+          onClick={ () => dispatch( getPokemons(page)) }
+        >
+          Next
+        </button>
 
     </>
   )
