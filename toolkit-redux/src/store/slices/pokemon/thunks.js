1f22b4b117a0cc1873da33cd963a461e2113c115
Edwin <edwin.correo.g@gmail.com>
2023-09-14 11:01:02 -0400

    vid 265 creacion de thunks

---
+++ toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -0,0 +1,8 @@
+import { startLoadingPokemons } from "./pokemonSlice"
+
+
+export const getPokemons = ( page= 0 ) => {
+    return async( dispatch, getState ) => {
+        dispatch( startLoadingPokemons() );
+    }
+}
