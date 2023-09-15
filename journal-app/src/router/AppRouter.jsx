aba264d896c01209e3fea70fa7ed8888d92e7649
Edwin <edwin.correo.g@gmail.com>
2023-09-15 18:02:29 -0400

    vid 291 sin terminar

--- journal-app/src/router/AppRouter.jsx
+++ journal-app/src/router/AppRouter.jsx
@@ -1,19 +1,36 @@
 import { Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { CheckingAuth } from "../ui"
+import { useEffect } from "react"
+import { onAuthStateChanged } from "firebase/auth"
+import { FirebaseAuth } from "../firebase/config"
+import { login, logout } from "../store/auth/authSlice"
 
 
 
 export const AppRouter = () => {
 
   const { status } = useSelector( state => state.auth );
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    onAuthStateChanged( FirebaseAuth, async( user ) =>{
+      if ( !user ) return dispatch( logout() );
+      const { uid, email, displayName, photoURL } = user;
+      dispatch( login({ uid, email, displayName, photoURL }) );
+
+    } )
+  
+  }, [])
+  
 
   if ( status === 'checking' ) {
     return <CheckingAuth />
   }
-  
+
   return (
     <Routes>
 
--- journal-app/src/ui/components/CheckingAuth.jsx
+++ journal-app/src/ui/components/CheckingAuth.jsx
@@ -12,7 +12,7 @@ export const CheckingAuth = () => {
     >
     
 
-        <Grid item
+        <Grid container
           direction={'row'}
           justifyContent={'center'}
         >
